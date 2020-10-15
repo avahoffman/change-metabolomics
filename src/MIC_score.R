e323df9e4d61f5a1582057ceffff8dd490e6106e
Ava Hoffman <avamariehoffman@gmail.com>
2020-10-15 12:34:38 -0400

    Add annotation to MIC score fxn

--- src/MIC_score.R
+++ src/MIC_score.R
@@ -10,7 +10,8 @@ library(minerva)
 library(foreach)
 library(doParallel)
 
-cl <- makeCluster(2)
+# Note that 24 is a lot of nodes! This is meant to be run on a high memory cluster.
+cl <- makeCluster(24)
 registerDoParallel(cl)
 
 ###########################################################################################
@@ -20,28 +21,47 @@ run_metab_data_MIC <-
   function(test_ = F,
            spp = 0,
            x_ = "nitrogen") {
+    # This function cycles through metabolites and calculates the MIC against a variable 
+    # of interest.
+    # test_: bool whether this should be a test run, with more reasonable criteria to do a
+    # debugging run on a laptop
+    # spp: 0 is bogr, 1 is spco
+    # x_: the x variable of interest across which the metabolite change will be calculated
+    # 
+    # output: writes a .csv containing pvalues and MIC scores for each metabolite for a 
+    # given x_
+    
+    # Read in metabolite data
     dat <-
       read.csv("data/clean_SpecAbund_metabolites.csv")
+    # Transpose data
     metabs <- as.data.frame(t(dat[, -c(1)]))
     
+    # Read in phys / nitrogen data
     phys_dat <-
       read.csv("data/phys_metabs_for_modules.csv")
     
+    # Set bootstraps and number of metabolites. If not testing, the loop should do all 
+    # of them
     if (!(test_)) {
+      # Full
       n_metabs_ <- ncol(metabs)
       nBootstraps = 5000
     } else {
+      # Test
       n_metabs_ <- 3
       nBootstraps = 5
     }
     
+    # Parallelize the tests across metabolites. Results will be row bound.
+    # Full version should be 1:(number of metabolites)
     spp_frame <-
       foreach(i = 1:n_metabs_, .combine = rbind) %dopar% {
-        #ncol(metabs)
         library(foreach)
         library(doParallel)
         library(minerva)
         
+        # Function to test for difference in null and actual MIC
         pcalc <-
           function(nullDist,
                    observedStatistic,
@@ -73,9 +93,13 @@ run_metab_data_MIC <-
             return(pValue)
           }
         
+        # Specific metabolite ID
         metab_id <- names(metabs[i])
-        print(metab_id)
+        
+        # Y variable is the metabolite in question
         yvar <- metabs[phys_dat$spp == spp, i]
+        
+        # X var depends on function argument
         if (x_ == "nitrogen") {
           xvar <- phys_dat$nitrogen[phys_dat$spp == spp]
         } else if (x_ == "photo") {
@@ -88,26 +112,35 @@ run_metab_data_MIC <-
           xvar <- phys_dat$iWUE[phys_dat$spp == spp]
         }
         
+        # Make sure that NAs are excluded, since they will interfere with the 
+        # test
         yvar_no_na <- yvar[!(is.na(xvar))]
         xvar_no_na <- xvar[!(is.na(xvar))]
         
+        # For each metabolite, you want to do null MIC draws to get a null
+        # distribution. This is what will be tested against the actual MIC
         MIC.null <- vector()
         for (b in 1:nBootstraps) {
           nsamples <- length(xvar_no_na)
           resampledX <-
             xvar_no_na[sample(1:nsamples, replace = F)]
-          mic <- mine(resampledX, yvar_no_na , n.cores = 2)
+          mic <- mine(resampledX, yvar_no_na, n.cores = 2)
           mic$MIC
+          # Append
           MIC.null[b] <- mic$MIC
         }
         
+        # Calculate actual MIC
         MIC.actual <- mine(xvar_no_na, yvar_no_na)$MIC
+        
+        # Test for a difference between Null and Actual
         p.value = pcalc(
           nullDist = MIC.null,
           observedStatistic = MIC.actual,
           sided = "one"
         )
         
+        # Assemble results in a dataframe
         out <-
           data.frame(
             metab_id = rep(metab_id, length(xvar_no_na)),
@@ -121,9 +154,11 @@ run_metab_data_MIC <-
         # Have to explicitly name this column to the variable
         colnames(out)[3] <- x_
         
+        # Output the dataframe so that results can be appended onto one another
         out
       }
     
+    # Write results in a .csv that depends on the x_ in question
     if (x_ == "nitrogen") {
       write.csv(spp_frame,
                 file = paste(spp, "_metabolomic_MIC_output_nitrogen.csv", sep = ""))
