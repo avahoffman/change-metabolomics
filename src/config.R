d5a89352d7ef5f893dbb80ebb2301397dcf176fd
Ava Hoffman <avamariehoffman@gmail.com>
2021-02-12 12:04:36 -0500

    Improve documentation on config

--- src/config.R
+++ src/config.R
@@ -3,19 +3,26 @@
 ###########################################################################################
 
 # PCA plotting
+
+# Which PC component should be used? Choose PC1, PC2, or PC3
 pca_component <- "PC1"
+
+# What should the y axis label be?
 pca_yaxis <- "PC1 (40%)"
 
 ###########################################################################################
 
 # MCMC sampling
+
+# How many iterations to use? The functions that use this variable will discard the first 
+# half of iter as warmup.
 iter <- 100000
 
 ###########################################################################################
 
 # Plotting
 
-# Follow viridis plotting convention
+# Follow viridis plotting convention, but these can be any hex color.
 bogr_color <- "#73D055FF"
 spco_color <- "#482677FF"
 elel_color <- "#287D8EFF"
@@ -25,12 +32,12 @@ bogr_shape <- 16
 spco_shape <- 8
 elel_shape <- 3
 
-# Species names
+# Species names - ensures these are italicized throughout
 B.gracilis <- expression(italic("B. gracilis"))
 E.elymoides <- expression(italic("E. elymoides"))
 S.coccinea <- expression(italic("S. coccinea"))
 
-# Labels for physiology plot
+# Labels for physiology plot y axes
 photo_ylab <-
   expression(atop("Net photsynthetic rate",
           "("~mu~"mol "~CO[2] ~ m ^ -2~s ^ -1~ ")"))
@@ -59,9 +66,7 @@ heatmap_plotnames <-
 heatmap_colorpal <- 
   colorRampPalette(c("#38598CFF","white","#92D741FF"))
 
-# Color palette for the SEM plot
-sem_pos_color <- "#92D741FF"
-sem_neg_color <- "#38598CFF"
-
-# Metabolites from MIC also in module 16
+# Metabolites from MIC also in module 16 - Currently this needs to be determined manually
+# by looking at metabolites with significant MIC output and metabolites found in module 16.
+# TODO: Make this automatic
 overlap_metab <- as.factor(c("V224","V713"))
